e33de91dbf53b6e64e6520e2f6498b5f53f7fbae
Shivom Sharma <81389102+RealShivomSharma@users.noreply.github.com>
2022-10-15 19:04:30 -0400

    Created setters/added new script
    Created setters for parameters (not sure if they work) made a script that works for forms. Not sure if it will be in final submission.

--- DCM/databases.py
+++ DCM/databases.py
@@ -2,6 +2,8 @@ from . import database #Importing database sqlAlchemy
 from flask_login import UserMixin #Gives user object flask login parameters
 from sqlalchemy.sql import func
 from flask_wtf import FlaskForm 
+from flask_login import current_user
+
 
 class Note(database.Model):
     id = database.Column(database.Integer, primary_key =True)
@@ -10,11 +12,10 @@ class Note(database.Model):
 Pacing class:
 Contains the database entries for all of the pacing modes, it contains the user_id as a Foreign Key
 This means that there is a one-to-many relationship I.E. "One user has many pacing modes and parameters associated"
-Contains all programmable parameters for the pacemaker that are inputted through the DCM interface
+Contains all programmable parameters for the pacemaker that are inputted through the DCM i  nterface
 """
 class Pacing(database.Model):
     user_id = database.Column(database.Integer, database.ForeignKey('user.id'))
-
     id = database.Column(database.Integer, primary_key = True)
     mode = database.Column(database.String(4), unique = True)
     LRL = database.Column(database.Integer)
@@ -44,3 +45,44 @@ Returns the number of users based on a query of the number of rows in database h
 """
 def countUsers(database):
     return database.session.query(User).count()
+"""
+VARIOUS SETTER FUNCTIONS TO SET PARAMETERS IN DATABASE
+"""
+
+def setMode(database, mode_input):
+    Pacing(mode = mode_input, user_id = current_user.id)
+    database.session.commit() 
+
+def setLRL(database, LRL_input):
+    Pacing(LRL = LRL_input, user_id = current_user.id)
+    database.session.commit() 
+
+def setURL(database, URL_input):
+    Pacing(LRL = URL_input, user_id = current_user.id)
+    database.session.commit() 
+
+def setATR_AMP(database, ATR_AMP_input):
+    Pacing(ATR_AMP = ATR_AMP_input, user_id = current_user.id)
+    database.session.commit() 
+
+def setVENT_AMP(database, VENT_AMP_input):
+    Pacing(VENT_AMP = VENT_AMP_input, user_id = current_user.id)
+    database.session.commit() 
+
+def setATR_PW(database, ATR_PULSE_WIDTH_input):
+    Pacing(ATR_PULSE_WIDTH = ATR_PULSE_WIDTH_input, user_id = current_user.id)
+    database.session.commit() 
+
+def setVENT_PW(database, VENT_PULSE_WIDTH_input):
+    Pacing(VENT_PULSE_WIDTH = VENT_PULSE_WIDTH_input, user_id = current_user.id)
+    database.session.commit() 
+
+def setVRP(database, VRP_input):
+    Pacing(VRP = VRP_input, user_id = current_user.id)
+    database.session.commit() 
+
+def setARP(database, ARP_input):
+    Pacing(ARP = ARP_input, user_id = current_user.id)
+    database.session.commit() 
+
+
